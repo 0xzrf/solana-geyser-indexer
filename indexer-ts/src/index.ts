7f70b8f7d6daf329c2a0e8911556d6ceb5bbd409
0xzrf <krishh16@yahoo.com>
2025-08-11 09:54:42 +0530

    added the code example to get blockchain data in low latency for free

--- indexer-ts/src/index.ts
+++ indexer-ts/src/index.ts
@@ -2,7 +2,7 @@ import Client, {
     SubscribeRequest,
     SubscribeUpdate,
   } from "@triton-one/yellowstone-grpc";
-  
+
 const client = new Client(
     "https://solana-rpc.parafi.tech:10443",
     undefined,
@@ -14,5 +14,55 @@ const client = new Client(
   }
 
 const run = async () => {
+    const stream = await client.subscribe();
+    const streamClosed = new Promise<void>((resolve, reject) => {
+        stream.on("error", (error) => {
+          reject(error);
+          stream.end();
+        });
+        stream.on("end", () => resolve());
+        stream.on("close", () => resolve());
+      });
 
+      stream.on("data", handleUpdate);
+      const subscribeRequest: SubscribeRequest = {
+        accounts: {},
+        slots: {},
+        transactions: {
+          group: {
+            accountInclude: ["So11111111111111111111111111111111111111112"],
+            accountExclude: [],
+            accountRequired: [],
+          },
+        },
+        transactionsStatus: {
+          group: {
+            accountInclude: ["So11111111111111111111111111111111111111112"],
+            accountExclude: [],
+            accountRequired: [],
+            failed: false
+          },
+        },
+        blocks: {},
+        blocksMeta: {},
+        entry: {},
+        accountsDataSlice: [],
+        commitment: 1
+      };
+    
+      // subscribe
+      await new Promise<void>((resolve, reject) => {
+        stream.write(subscribeRequest, (error: any) => {
+          if (error) reject(error);
+          else resolve();
+        });
+      }).catch((reason) => {
+        console.error(reason);
+        throw reason;
+      });
+    
+      await streamClosed;
 }
+
+
+run()
